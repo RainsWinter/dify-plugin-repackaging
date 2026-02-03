a8046ff857f7d30d83ee48115bb120c2ac57ff0d
RainsWinter <52773311+RainsWinter@users.noreply.github.com>
2026-02-03 13:25:13 +0800

    Change pip download source to use PIP_MIRROR_URL

--- plugin_repackaging.sh
+++ plugin_repackaging.sh
@@ -110,7 +110,7 @@ repackage(){
 	echo "Unzip success."
 	echo "Repackaging ..."
 	cd ${CURR_DIR}/${PACKAGE_NAME}
-	pip download -r requirements.txt -d ./wheels --index-url https://pypi.org/simple
+	pip download -r requirements.txt -d ./wheels --index-url ${PIP_MIRROR_URL} --trusted-host mirrors.aliyun.com
 	if [[ $? -ne 0 ]]; then
 		echo "Pip download failed."
 		exit 1
